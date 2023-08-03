9b9a24873451408b870ce4ebc32703e46aba7c75
Guilherme Schulz <gui.schulz@gmail.com>
2023-08-03 10:10:59 -0300

    fixed logo to redirect to home

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -9,6 +9,7 @@ import { useRouter } from 'next/navigation'
 import { useAppDispatch } from '@/store/redux';
 import { setSearchedPokemon } from '@/reducers/pokemonSearchReducer';
 import { toast } from 'react-toastify';
+import Link from 'next/link';
 
 
 export const Header = () => {
@@ -48,10 +49,10 @@ export const Header = () => {
 
     return (
         <header className="p-5 flex justify-between items-center border-b-2 border-carmine bg-slate-950 max-sm:flex-col max-sm:gap-4">
-            <div className="flex items-center gap-5">
+            <Link href={"/"} className="flex items-center gap-5">
                 <Image src={logo} className="w-[30px]" alt="PokeProject Logo" />
                 <h1>PokeProject</h1>
-            </div>
+            </Link>
             <div className="flex items-center w-[20%] h-10 max-sm:w-[80%]">
                 <input onChange={(e) => setSearch(e.target.value)} className='h-[99%] w-[90%] bg-white rounded-l-sm px-2 text-black focus:outline-none' placeholder='Search Pokemon Here' id='search' type="text" name='Search' />
                 <button onClick={() => requestPoke()} className='bg-white rounded-r-sm px-2 text-black h-[99%] flex items-center justify-center' ><SearchIcon /></button>
